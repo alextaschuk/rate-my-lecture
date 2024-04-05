c78634f50f7a8b27126a69c48ff0d94919fc721e
Sam <sikora.samj@gmail.com>
2024-04-05 19:00:35 +0000

    Fix Invaild DOM Property

--- src/app/components/selectCampus/SelectCampus.tsx
+++ src/app/components/selectCampus/SelectCampus.tsx
@@ -22,7 +22,7 @@ const CampusDropdown: React.FC = () => {
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
             viewBox="0 0 24 24"
-            stroke-width="1.5"
+            strokeWidth="1.5"
             stroke="currentColor"
             className="w-6 h-6"
           >
